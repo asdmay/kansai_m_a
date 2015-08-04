e294db42a9035a527ddc2b995471c83e43c2637c
asdmay <wrbmc.mc.pr@gmail.com>
2015-08-05 02:12:21 +0900

    地図を表示、マーカー移動ができるようになった

--- js/map.js
+++ js/map.js
@@ -1,26 +1,41 @@
 var message;
 var cityCircle;
 
+var citymap = {};
+citymap['USJ'] = {
+    center: new google.maps.LatLng(34.664722, 135.433056),//緯度,経度
+    takumi_p: 10
+};
+citymap['海遊館'] = {
+    center: new google.maps.LatLng(34.654472, 135.428889),
+    takumi_p: 20
+};
+citymap['大阪市立科学館'] = {
+    center: new google.maps.LatLng(34.691306, 135.491583),
+    takumi_p: 30
+};
+citymap['交通科学博物館'] = {
+    center: new google.maps.LatLng(34.670715, 135.461895),
+    takumi_p: 40
+};
+
+
 function start_func(){
-	get_location();
+    get_location();
 }
-// ( 1 )位置情報を取得します。
+
 function get_location(){
-	if (navigator.geolocation) {
-        // 現在の位置情報取得を実施 正常に位置情報が取得できると、
-        // successCallbackがコールバックされます。
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition
         (successCallback,errorCallback);
     } else {
         console.log("error");
     }
 }
-// ( 2 )位置情報が正常に取得されたら
-function successCallback(pos) {
-	var Potition_latitude = pos.coords.latitude; //緯度
-	var Potition_longitude = pos.coords.longitude;　//軽度
 
-    // 位置情報が取得出来たらGoogle Mapを表示する
+function successCallback(pos) {
+    var Potition_latitude = pos.coords.latitude; //緯度
+    var Potition_longitude = pos.coords.longitude;//軽度
     initialize(Potition_latitude,Potition_longitude);
 }
 
@@ -28,59 +43,51 @@ function errorCallback(error) {
     console.log("error");
 }
 
-// ( 3 )Google Map API を使い、地図を読み込み
 function initialize(x,y) {
-
-    // Geolocationで取得した座標を代入
-    // FirefoxOS端末及びシミュレータが現在地をただしくとれないため一時的に固定
+    
     var myLatlng = new google.maps.LatLng(34.664722, 135.433056);//USJ
-    // var myLatlng = new google.maps.LatLng(34.706193, 135.494145);//総情
-    //var myLatlng = new google.maps.LatLng(x,y);
-
+    //var myLatlng = new google.maps.LatLng(x,y); 
+    
     var mapOptions = {
     	zoom: 14,
     	center: myLatlng,
     	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    // MapTypeId に、地図タイプを指定
-    // HYBRID 衛星画像と主要な通りが表示されます
-    // ROADMAP 通常の地図画像が表示されます
-    // SATELLITE 衛生画像が表示されます。
-    // TERRAIN 地形や植生などのマッピングをします。
-
+    
     var map = new google.maps.Map
     (document.getElementById("map_canvas"), mapOptions);
-
-
+    
+    for (var city in citymap) {
         var populationOptions = {
-            //strokeColor: red,
+            strokeColor: '#FF0000',
             strokeOpacity: 0.8, //透明度
             strokeWeight: 2,
-            //fillColor: red,
+            fillColor: '#FF0000',
             fillOpacity: 0.35,
             map: map,
-            center: new google.maps.LatLng(34.664722, 135.433056),
+            center: citymap[city].center,
             radius: 50
         };
         // マップへcityのための円を加える
         cityCircle = new google.maps.Circle(populationOptions);
     }
-
     //現在地にマーカーを置く
-var marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
     	position: myLatlng,
     	map: map,
     	title:"Your position",
 	draggable: true//マーカーを動かせるようにする
     });
-    google.maps.event.addListener(marker, 'dragend', function(ev){
+    
+}
+   google.maps.event.addListener(marker, 'dragend', function(ev){
 	updateHajiraiPoint();
     });
 
     function checkHajiraiPoint(lat, lng) {
 	var min = Number.MAX_VALUE;
 	var min_i = 0;
-	
+
 	for( var i = 0; i < citymap.length; i++){
 	    var distance = google.maps.geometry.spherical.computeDistanceBetween(citymap[i].center, new google.maps.LatLng(lat, lng));
   	    if( min > distance ){
@@ -91,7 +98,7 @@ var marker = new google.maps.Marker({
 	var radius = Math.sqrt(citymap[min_i].area/Math.PI);
 	var point = 0;
 	if(min<radius){
-	    point = citymap[min_i].population;
+	    point = citymap[min_i].point;
 	}
 	return point;
     }
@@ -107,6 +114,7 @@ var marker = new google.maps.Marker({
 	document.getElementById("show_point").innerHTML = point;
     }
 
-    updateHajiraiPoint();
+updateHajiraiPoint();
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
